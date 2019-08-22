eb6ca83d11a3bc610ed7ba6708941e58e820e00c
Tim Drysdale <timothy.d.drysdale@gmail.com>
2019-08-22 15:56:04 +0100

    command expansion passing tests

--- cmd/startcapture.go
+++ cmd/startcapture.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"net/url"
+	"os"
 )
 
 //func unmarshalConfig(v *viper.Viper, o interface{}) error {
@@ -32,44 +33,33 @@ func populateInputNames(o *Output) {
 }
 
 func mapEndpoints(o Output, h *url.URL) Endpoints {
-
 	//go through feeds to collect inputs into map
 
 	var e = make(Endpoints)
 
 	for _, v := range o.Streams {
 		for _, f := range v.InputNames {
-			//fmt.Println("\t", f)
 			e[f] = constructEndpoint(h, f)
-
 		}
 	}
 
 	return e
-
 }
 
-func expandCaptureCommands(o interface{}, c interface{}) error {
-	//
-	//	if err != nil {
-	//		fmt.Println("Didnt unpack streams config")
-	//		return err
-	//	} else {
-	//		for _, stream := range outs.Streams {
-	//			fmt.Printf("destination:%v\n", stream.Destination)
-	//			for _, name := range stream.InputNames {
-	//				inputAddresses[name] = fmt.Sprintf("%s/%s/", listen, name)
-	//				fmt.Printf("%v\v", inputAddresses[name])
-	//
-	//			} //for
-	//
-	//		} //for
-	//
-	//		for _, name := range inputAddresses {
-	//			inputChannels[name] = make(chan Packet)
-	//			fmt.Printf("%s:%s\n", name, inputAddresses[name])
-	//		}
-	//
-	//	} //else
-	return nil
+func expandCaptureCommands(c *Commands, e Endpoints) {
+
+	// we rely on e being in scope for the mapper when it runs
+	mapper := func(placeholderName string) string {
+		if val, ok := e[placeholderName]; ok {
+			return val
+		} else {
+			return placeholderName //don't change what we don't know about
+		}
+
+	}
+
+	for i, raw := range c.Commands {
+		c.Commands[i] = os.Expand(raw, mapper)
+	}
+
 }
