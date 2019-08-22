8132a5ef3f9ee430d4b4ae4eae0fe3b2e3a4875a
Tim Drysdale <timothy.d.drysdale@gmail.com>
2019-08-22 17:03:25 +0100

    added command execution with working kill

--- cmd/startcapture_test.go
+++ cmd/startcapture_test.go
@@ -2,10 +2,13 @@ package cmd
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"net/url"
 	"strings"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -80,6 +83,18 @@ var expectedEndpoints = Endpoints{"videoFrontMedium": "http://127.0.0.1:8080/vid
 	"videoFrontSmall": "http://127.0.0.1:8080/videoFrontSmall",
 	"videoSideMedium": "http://127.0.0.1:8080/videoSideMedium",
 	"videoSideSmall":  "http://127.0.0.1:8080/videoSideSmall"}
+
+var cmd0 = "sleep 0.1"
+var cmd1 = "sleep 0.2"
+var cmd2 = "sleep 0.3"
+
+var testCommands = Commands{Commands: []string{cmd0, cmd1, cmd2}}
+
+var cmdSlow0 = "sleep 1"
+var cmdSlow1 = "sleep 2"
+var cmdSlow2 = "sleep 3"
+
+var testCommandsSlow = Commands{Commands: []string{cmdSlow0, cmdSlow1, cmdSlow2}}
 
 func TestExpandCaptureCommands(t *testing.T) {
 
@@ -176,4 +191,37 @@ func TestExpandCaptureCommands(t *testing.T) {
 		}
 	}
 
+	// try sleeping in external processes
+	closed := make(chan struct{})
+	var wg sync.WaitGroup
+	wg.Add(1)
+	start := time.Now()
+	runCaptureCommands(closed, &wg, testCommands)
+	elapsed := time.Since(start)
+	if elapsed < 300*time.Millisecond {
+		t.Errorf("running sleep commands was too short %v", elapsed)
+	}
+	if elapsed > 350*time.Millisecond {
+		t.Errorf("running sleep commands took too long %v", elapsed)
+	}
+
+	//try killing processes via closed
+	wg.Add(1)
+	go func() {
+		fmt.Println("in closer")
+		select {
+		case <-time.After(500 * time.Millisecond):
+			close(closed)
+		}
+	}()
+
+	start = time.Now()
+	runCaptureCommands(closed, &wg, testCommandsSlow)
+	elapsed = time.Since(start)
+	if elapsed < 500*time.Millisecond {
+		t.Errorf("killing sleep commands was too short %v", elapsed)
+	}
+	if elapsed > 1000*time.Millisecond {
+		t.Errorf("killing sleep commands took too long %v", elapsed)
+	}
 }
