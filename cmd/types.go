b09fc250b204b12f08ceeddd9f0053a3d03188b9
Tim Drysdale <timothy.d.drysdale@gmail.com>
2019-08-27 23:47:22 +0100

    added files from crossbar

--- cmd/types.go
+++ cmd/types.go
@@ -1,5 +1,11 @@
 package cmd
 
+import (
+	"sync"
+
+	"github.com/gobwas/ws"
+)
+
 type Commands struct {
 	Commands []string `yaml:"commands"`
 }
@@ -47,4 +53,46 @@ type MuxOptions struct {
 type HTTPOptions struct {
 	Port   int `yaml:"port"`
 	WaitMS int `yaml:"waitMS"`
+}
+
+// Types from crossbar
+
+// messages will be wrapped in this struct for muxing
+type message struct {
+	sender clientDetails
+	op     ws.OpCode
+	data   []byte //text data are converted to/from bytes as needed
+}
+
+type clientDetails struct {
+	name         string
+	topic        string
+	messagesChan chan message
+}
+
+// requests to add or delete subscribers are represented by this struct
+type clientAction struct {
+	action clientActionType
+	client clientDetails
+}
+
+// userActionType represents the type of of action requested
+type clientActionType int
+
+// clientActionType constants
+const (
+	clientAdd clientActionType = iota
+	clientDelete
+)
+
+type topicDirectory struct {
+	sync.Mutex
+	directory map[string][]clientDetails
+}
+
+// gobwas/ws
+type readClientDataReturns struct {
+	msg []byte
+	op  ws.OpCode
+	err error
 }
